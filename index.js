d0b2e27b31b6393ae202dea4291a55c12b512d92
Tim Harshman <goteamtim+git@gmail.com>
2016-03-22 23:49:30 -0700

    Added Google distance matrix API

--- index.js
+++ index.js
@@ -13,15 +13,28 @@ function updateWeatherData(){
     })
 };
 
+function getCurrentDriveTime(){
+    request('https://maps.googleapis.com/maps/api/distancematrix/json?origins={long},{lat}&destinations={long},{lat}&departure_time=now&traffic_model=best_guess&key={googleDistanceApiKey}', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            
+            var currentTime = new Date().getTime();
+            console.log("Got Drive Time: " + currentTime);
+            return body;
+        }
+        //handle error.  
+    })
+};
+};
+
 updateWeatherData();
 setInterval(updateWeatherData,60000*60);
 
 // we are specifying the html directory as another public directory
 app.use(express.static(__dirname));
 
-   
-app.get('/mirror',function(req,res){
- res.sendFile(__dirname + '\/mirror.htm'); 
+   //Impliment users?
+app.get('/mirror/:userName',function(req,res){
+ res.sendFile(__dirname + '\/mirror.htm?userID='+ req.params.userName); 
 });
 
 app.get('/setup',function(req,res){
@@ -30,6 +43,10 @@ app.get('/setup',function(req,res){
 
 app.get('/weather', function(req, res) {
     res.send(userData);
+});
+
+app.get('/driveTime',function(req,res){
+    res.send(getCurrentDriveTime());
 });
 
 app.listen(3000, function() {
