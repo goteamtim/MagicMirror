13bf421f8b16604ec38da6910c964c9ec65e6cfe
Tim Harshman <goteamtim+git@gmail.com>
2016-03-22 22:54:30 -0700

    Added route to mirror and setup

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var app = express();
+var request = require('request');
+var fs = require('fs');
 
-app.get('/', function (req, res) {
-  res.send('This is my first server checkin and this is the best I could come up with.  Listening to Pink Floyd - Comfortably Numb ;)');
+// we are specifying the html directory as another public directory
+app.use(express.static(__dirname));
+
+
+//
+
+app.get('/mirror',function(req,res){
+
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/weather', function(req, res) {
+    request('https://api.forecast.io/forecast/APIKEY/LATITUDE,LONGITUDE', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body) // Show the HTML for the Google homepage.
+        }
+    })
+    res.send();
+});
+
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
 });
