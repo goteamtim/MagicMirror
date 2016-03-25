94f97c45722eb130ac2571893dc48b8822e47464
Tim Harshman <goteamtim+git@gmail.com>
2016-03-25 01:16:49 -0700

    Updated the logo, Added Random Quote, added weather description

--- index.js
+++ index.js
@@ -2,51 +2,96 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var fs = require('fs');
-var userData = {};
+var userData = {
+    ip_info: {},
+    weather: {}
+};
+var quote = {};
 
-function updateWeatherData(){
-    request('https://api.forecast.io/forecast/{apiKey}/{lat},{long}', function(error, response, body) {
+function updateWeatherData() {
+    //Check if the ip information exists and if it doesnt call the function and wait
+    if (!userData["ip_info"].hasOwnProperty("loc")) {
+        getUserLocation();
+        setTimeout(updateWeatherData, 5000);
+        return;
+    }
+    console.log("test" + userData["ip_info"].loc);
+    request('https://api.forecast.io/forecast/{apiKey}/' + userData["ip_info"].loc, function(error, response, body) {
         if (!error && response.statusCode == 200) {
-            userData = body;
+            userData["weather"] = body;
             console.log("Stored userData.");
+        } else { console.log("API call to weather not working.\n" + error); }
+    })
+};
+
+function getUserLocation() {
+    request('http://ipinfo.io', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            userData["ip_info"] = JSON.parse(body);
+        } else {
+            console.log("Error in IP: " + error);
         }
     })
 };
 
-function getCurrentDriveTime(){
+function getCurrentDriveTime() {
     request('https://maps.googleapis.com/maps/api/distancematrix/json?origins={long},{lat}&destinations={long},{lat}&departure_time=now&traffic_model=best_guess&key={googleDistanceApiKey}', function(error, response, body) {
         if (!error && response.statusCode == 200) {
-            
-            var currentTime = new Date().getTime();
-            console.log("Got Drive Time: " + currentTime);
-            return body;
+
+            if (body.status != "REQUEST_DENIED") {
+                var currentTime = new Date().getTime();
+                console.log("Drive time status: " + body.status)
+                console.log("Got Drive Time: " + currentTime);
+                return body;
+            } else {
+                console.log("Drive time status: " + body.status);
+                return body.status;
+            }
         }
         //handle error.  
     })
 };
 
+function getRandomQuote() {
+    request('http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+            var parsedQuote = JSON.parse(body);
+            console.log("---" + parsedQuote.quoteText);
+            return parsedQuote;
+        } else {
+            console.log("Error in quote: " + error);
+        }
+    })
+};
 
 updateWeatherData();
-setInterval(updateWeatherData,60000*60);
+setInterval(updateWeatherData, 60000 * 60);
+getRandomQuote();
+getUserLocation();
 
 // we are specifying the html directory as another public directory
 app.use(express.static(__dirname));
 
-   //Impliment users?
-app.get('/mirror/:userName',function(req,res){
- res.sendFile(__dirname + '\/mirror.htm?userID='+ req.params.userName); 
+//Impliment users?
+app.get('/mirror/:userName', function(req, res) {
+    res.sendFile(__dirname + '\/mirror.htm?userID=' + req.params.userName);
 });
 
-app.get('/setup',function(req,res){
+app.get('/setup', function(req, res) {
     res.sendFile(__dirname + '\/setup.htm');
 });
 
 app.get('/weather', function(req, res) {
-    res.send(userData);
+    res.send(userData["weather"]);
 });
 
-app.get('/driveTime',function(req,res){
+app.get('/driveTime', function(req, res) {
     res.send(getCurrentDriveTime());
+});
+
+app.get('/randomQuote', function(req, res) {
+    res.send(getRandomQuote());
 });
 
 app.listen(3000, function() {
--- js/mirror.js
+++ js/mirror.js
@@ -2,37 +2,93 @@ var apiKey;
 var zip;
 var weatherData = {};
 var userDataString = getCookie('userData');
-var temperatureUnits = 'Imperial';
+var weatherLastLoadTime = Date.now();
 //var userData = JSON.parse(userDataString);
 
-function loadWeatherData(){
-	$.getJSON("http://localhost:3000/weather", function(json){
-		//Gather weather here as object first then use it later in broken out functuions?
-			weatherData = json;
-            //document.querySelector("#locationName").innerHTML = json.name;
-            document.querySelector("#curr_temp").innerHTML = json.currently.apparentTemperature;
-            //document.querySelector("#temp_min").innerHTML = json.main.temp_min;
-            
-	})
+function loadWeatherData() {
+    var currentTime = Date.now();
+    //Dont get the weather if called within two hours for now
+    if (weatherLastLoadTime + 720000 > currentTime) {
+        $.getJSON("http://localhost:3000/weather", function(json) {
+            //Gather weather here as object first then use it later in broken out functuions?
+            weatherData = json;
+            storeCookie(JSON.stringify(json),'weatherData');
+            document.querySelector("#currTemp").innerHTML = Math.round(json.currently.apparentTemperature);
+            document.querySelector("#currDesc").innerHTML = json.hourly.summary;
+        })
+    } else {
+
+    }
+
 }
 
-function getCookie(name){
-	//sanitizeName
-	name += "=";
-	//break cookies into array
-	var cookies = document.cookie.split(';');
-	//cycle through array until you find the cookie then break the function and return the stored value
-	for(var i = 0; i < cookies.length; i++){
-		var cookie = cookies[i];
-		if(cookie.substring(1,cookie.indexOf('=')+1) == name){
-			return cookie.substring(cookie.indexOf('=')+1,cookie.length);
-		}
-	}
+function refreshQuote(){
+    $.getJSON("http://localhost:3000/randomQuote", function(json) {
+        console.log(json);
+            document.querySelector("#quote").innerHTML = json.quoteText;
+            document.querySelector("#quoteAuthor").innerHTML = json.quoteAuthor;
+        })
+};
+
+function updateDate() {
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    var n = weekday[d.getDay()];
+    document.getElementById('today').innerHTML = n;
 }
+
+function getCookie(name) {
+    //sanitizeName
+    name += "=";
+    //break cookies into array
+    var cookies = document.cookie.split(';');
+    //cycle through array until you find the cookie then break the function and return the stored value
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        if (cookie.substring(1, cookie.indexOf('=') + 1) == name) {
+            return cookie.substring(cookie.indexOf('=') + 1, cookie.length);
+        }
+    }
+}
+
+var storeCookie = function(cookieData, source) {
+    //Remember if cookie data doesnt wor you changed this function
+    if (source == null) {
+        document.cookie = cookieData;
+    } else {
+        document.cookie = source + '=' + cookieData;
+    }
+
+}
+
 
 loadWeatherData()
+updateDate()
+refreshQuote()
 //Google driving api maybe?
 //Uber waiting time
 //Top stories
 //Random uplifting quote from reddit?  /r/uplifting stories?  /r/quotes?
 //Calendar
+
+var skycons = new Skycons({ "color": "white" });
+// on Android, a nasty hack is needed: {"resizeClear": true}
+
+// you can add a canvas by it's ID...
+skycons.add("clear-night", Skycons.PARTLY_CLOUDY_NIGHT);
+
+// ...or by the canvas DOM element itself.
+skycons.add(document.getElementById("icon2"), Skycons.RAIN);
+
+// if you're using the Forecast API, you can also supply
+// strings: "partly-cloudy-day" or "rain".
+
+// start animation!
+skycons.play();
