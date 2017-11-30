4af16748d6118a8508af70c4672bd85e73e33164
Tim Harshman <goteamtim+git@gmail.com>
2017-11-29 21:43:51 -0800

    Tried build types pretty sure this isnt going to work because I need another build task to fire for the pord test task but im tired.

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,17 @@ var clean = require('gulp-clean');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
 
+// Default task - stage build
+gulp.task('default', ['browsersync-dev', 'watch-dev']);
 
-// Static server
-gulp.task('browsersync', ['nodemon'], function () {
+gulp.task('watch-dev', function () {
+
+  gulp.watch(['**.**', 'js/*','css/*'], reload);
+
+});
+
+// Static dev server
+gulp.task('browsersync-dev', ['nodemon-dev'], function () {
   browserSync.init({
     proxy: "localhost:3000",  // local node app address
     port: 5000,  // use *different* port than above
@@ -18,7 +26,7 @@ gulp.task('browsersync', ['nodemon'], function () {
   //gulp.watch("*.*").on('change',function(){console.log("gulp watched")});  //reload
 });
 
-gulp.task('nodemon', function (cb) {
+gulp.task('nodemon-dev', function (cb) {
   var called = false;
   return nodemon({
     script: 'index.js',
@@ -40,18 +48,46 @@ gulp.task('nodemon', function (cb) {
     });
 });
 
-// Default task
-gulp.task('default', ['browsersync', 'watch']);
+//prod build for testing
+gulp.task('test prod', ['build','browsersync-prod', 'watch-prod']);
 
-gulp.task('watch', function () {
+gulp.task('watch-prod', function () {
 
   gulp.watch(['**.**', 'js/*','css/*'], reload);
 
 });
 
-gulp.task('clean',function(){
-  return gulp.src(['public/*'], {read:false})
-  .pipe(clean());
+// Static dev server - prod build
+gulp.task('browsersync-prod', ['nodemon-prod'], function () {
+  browserSync.init({
+    proxy: "localhost:3000",  // local node app address
+    port: 5000,  // use *different* port than above
+    notify: true
+  });
+
+  //gulp.watch("*.*").on('change',function(){console.log("gulp watched")});  //reload
+});
+
+gulp.task('nodemon-prod', function (cb) {
+  var called = false;
+  return nodemon({
+    script: 'index.js',
+    ignore: [
+      'gulpfile.js',
+      'node_modules/'
+    ]
+  })
+    .on('start', function () {
+      if (!called) {
+        called = true;
+        cb();
+      }
+    })
+    .on('restart', function () {
+      setTimeout(function () {
+        reload({ stream: false });
+      }, 1000);
+    });
 });
 
 //Build Prod
@@ -68,3 +104,5 @@ gulp.task('compress', function () {
     console.log("Compress complete...")
   );
 });
+
+//test prod
