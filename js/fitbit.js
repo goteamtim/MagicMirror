3a4628aed3c9a569267db2d94e9f417d7766bcfd
Tim Harshman <goteamtim+git@gmail.com>
2017-12-26 13:52:42 -0800

    clean JS

--- js/fitbit.js
+++ js/fitbit.js
@@ -1,5 +1,3 @@
 var urlHash = window.location.hash;
 var hashedUrl = window.location;
-console.log(window.location)
-console.log(urlHash.replace('#','?'))
 window.location = window.location.origin + window.location.pathname + urlHash.replace('#','?')
