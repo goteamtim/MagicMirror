e9a1a6805c9171c323fc71b165fa27a46fcb00f1
Tim Harshman <goteamtim+git@gmail.com>
2017-12-27 23:51:39 -0800

    asdf

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ var hashedUrl = window.location;
 
 document.querySelector('#submit').addEventListener("click", function(){
 var url = document.querySelector('#input').value;
+  console.log("url: " + url)
   window.location = url + urlHash.replace('#','?')
 })
