deaffc8ae07ca705f832fa5b8e0430228fec2e30
Tim Harshman <goteamtim+git@gmail.com>
2017-12-27 23:54:56 -0800

    f

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 var urlHash = window.location.hash;
-var hashedUrl = window.location;
-
-
 document.querySelector('#submit').addEventListener("click", function(){
 var url = document.querySelector('#input').value;
-  console.log("url: " + url)
   window.location = url + urlHash.replace('#','?')
 })
